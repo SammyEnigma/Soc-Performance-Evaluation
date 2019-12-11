bd6d21ba6a991f373a9234919ff09ca099dd7ec7
MRXY001 <wxy19980615@gmail.com>
2019-12-11 09:57:54 +0800

    添加用户可自定义数据类型

---
+++ shapes/base/customdatatype.cpp
@@ -0,0 +1,183 @@
+/*
+ * @Author: MRXY001
+ * @Date: 2019-12-11 09:32:16
+ * @LastEditors: MRXY001
+ * @LastEditTime: 2019-12-11 09:56:52
+ * @Description: 用户自定义数据类型
+ */
+#include "customdatatype.h"
+
+CustomDataType::CustomDataType(QString name, bool val, bool def)
+{
+    this->name = name;
+    this->val = val;
+    this->def = def;
+    this->type = DT_BOOL;
+}
+
+CustomDataType::CustomDataType(QString name, int val, int def)
+{
+    this->name = name;
+    this->val = val;
+    this->def = def;
+    this->type = DT_INT;
+}
+
+CustomDataType::CustomDataType(QString name, double val, double def)
+{
+    this->name = name;
+    this->val = val;
+    this->def = def;
+    this->type = DT_DOUBLE;
+}
+
+CustomDataType::CustomDataType(QString name, QString val, QString def)
+{
+    this->name = name;
+    this->val = val;
+    this->def = def;
+    this->type = DT_STRING;
+}
+
+CustomDataType::CustomDataType(QString name, QStringList val, QStringList def)
+{
+    this->name = name;
+    this->val = val;
+    this->def = def;
+    this->type = DT_BOOL;
+}
+
+void CustomDataType::setName(QString name)
+{
+    this->name = name;
+}
+
+void CustomDataType::reset(bool val, bool def)
+{
+    this->val = val;
+    this->def = def;
+    this->type = DT_BOOL;
+}
+
+void CustomDataType::reset(int val, int def)
+{
+    this->val = val;
+    this->def = def;
+    this->type = DT_INT;
+}
+
+void CustomDataType::reset(double val, double def)
+{
+    this->val = val;
+    this->def = def;
+    this->type = DT_DOUBLE;
+}
+
+void CustomDataType::reset(QString val, QString def)
+{
+    this->val = val;
+    this->def = def;
+    this->type = DT_STRING;
+}
+
+void CustomDataType::reset(QStringList val, QStringList def)
+{
+    this->val = val;
+    this->def = def;
+    this->type = DT_STRING_LIST;
+}
+
+void CustomDataType::setValue(bool val)
+{
+    this->val = val;
+    this->type = DT_BOOL;
+}
+
+void CustomDataType::setValue(int val)
+{
+    this->val = val;
+    this->type = DT_INT;
+}
+
+void CustomDataType::setValue(double val)
+{
+    this->val = val;
+    this->type = DT_DOUBLE;
+}
+
+void CustomDataType::setValue(QString val)
+{
+    this->val = val;
+    this->type = DT_STRING;
+}
+
+void CustomDataType::setValue(QStringList val)
+{
+    this->val = val;
+    this->type = DT_BOOL;
+}
+
+void CustomDataType::setDefault(bool def)
+{
+	bool val_eq_def = (this->val == this->def);
+    this->def = def;
+    if (val_eq_def)
+        this->val = def;
+}
+
+void CustomDataType::setDefault(int def)
+{
+	bool val_eq_def = (this->val == this->def);
+    this->def = def;
+    if (val_eq_def)
+        this->val = def;
+}
+
+void CustomDataType::setDefault(double def)
+{
+	bool val_eq_def = (this->val == this->def);
+    this->def = def;
+    if (val_eq_def)
+        this->val = def;
+}
+
+void CustomDataType::setDefault(QString def)
+{
+	bool val_eq_def = (this->val == this->def);
+    this->def = def;
+    if (val_eq_def)
+        this->val = def;
+}
+
+void CustomDataType::setDefault(QStringList def)
+{
+	bool val_eq_def = (this->val == this->def);
+    this->def = def;
+    if (val_eq_def)
+        this->val = def;
+}
+
+QString CustomDataType::getName()
+{
+    return name;
+}
+
+DataType CustomDataType::getType()
+{
+    return type;
+}
+
+QVariant CustomDataType::getValue()
+{
+    return val;
+}
+
+QVariant CustomDataType::getDefault()
+{
+    return def;
+}
+
+QVariant &CustomDataType::value()
+{
+    return val;
+}
---
+++ shapes/base/customdatatype.h
@@ -0,0 +1,65 @@
+/*
+ * @Author: MRXY001
+ * @Date: 2019-12-11 09:32:16
+ * @LastEditors: MRXY001
+ * @LastEditTime: 2019-12-11 09:48:20
+ * @Description: 用户自定义数据类型
+ */
+#ifndef CUSTOMDATATYPE_H
+#define CUSTOMDATATYPE_H
+
+#include <QVariant>
+#include <QStringList>
+
+enum DataType {
+    DT_UNKNOW,
+    DT_BOOL,
+    DT_INT,
+    DT_DOUBLE,
+    DT_STRING,
+    DT_STRING_LIST
+};
+
+class CustomDataType
+{
+public:
+    CustomDataType(QString name, bool val, bool def = false);
+    CustomDataType(QString name, int val, int def = 0);
+    CustomDataType(QString name, double val, double def = 0.0);
+    CustomDataType(QString name, QString val, QString def = "");
+    CustomDataType(QString name, QStringList val, QStringList def=QStringList());
+
+    void setName(QString name);
+
+    void reset(bool val, bool def = false);
+    void reset(int val, int def = 0);
+    void reset(double val, double def = 0.0);
+    void reset(QString val, QString def = "");
+    void reset(QStringList val, QStringList def = QStringList());
+    
+    void setValue(bool val);
+    void setValue(int val);
+    void setValue(double val);
+    void setValue(QString val);
+    void setValue(QStringList val);
+
+    void setDefault(bool def = false);
+    void setDefault(int def = 0);
+    void setDefault(double def = 0.0);
+    void setDefault(QString def = "");
+    void setDefault(QStringList def = QStringList());
+
+    QString getName();
+    DataType getType();
+    QVariant getValue();
+    QVariant getDefault();
+    QVariant& value();
+
+private:
+    QString name;
+    DataType type;
+    QVariant val;
+    QVariant def;
+};
+
+#endif // CUSTOMDATATYPE_H
