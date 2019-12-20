97bad17d528b8944380acc83f3db2a170d6fd31d
MRXY001 <wxy19980615@gmail.com>
2019-12-20 12:45:19 +0800

    添加Hub变量

--- soc/flowcontrol/flowcontrol_master2_switch_slave2.cpp
+++ soc/flowcontrol/flowcontrol_master2_switch_slave2.cpp
@@ -13,25 +13,61 @@ FlowControl_Master2_Switch_Slave2::FlowControl_Master2_Switch_Slave2(GraphicArea
 
 bool FlowControl_Master2_Switch_Slave2::initModules()
 {
+    FlowControlBase::initModules();
 
+    hub = static_cast<SwitchModule *>(graphic->findShapeByClass("Switch"));
+    master1 = static_cast<MasterModule *>(graphic->findShapeByText("Master1"));
+    master2 = static_cast<MasterModule *>(graphic->findShapeByText("Master2"));
+    slave1 = static_cast<SlaveModule *>(graphic->findShapeByText("Slave1"));
+    slave2 = static_cast<SlaveModule *>(graphic->findShapeByText("Slave2"));
+    master1_cable = static_cast<ModuleCable *>(getModuleCable(master1, hub));
+    master2_cable = static_cast<ModuleCable *>(getModuleCable(master2, hub));
+    slave1_cable = static_cast<ModuleCable *>(getModuleCable(slave1, hub));
+    slave2_cable = static_cast<ModuleCable *>(getModuleCable(slave2, hub));
+    master1_port = static_cast<ModulePort *>(master1_cable->getFromPort());
+    master2_port = static_cast<ModulePort *>(master2_cable->getFromPort());
+    slave1_port = static_cast<ModulePort *>(slave1_cable->getToPort());
+    slave2_port = static_cast<ModulePort *>(slave2_cable->getToPort());
+    hm1_port = static_cast<ModulePort *>(master1_cable->getToPort());
+    hm2_port = static_cast<ModulePort *>(master2_cable->getToPort());
+    hs1_port = static_cast<ModulePort *>(slave1_cable->getFromPort());
+    hs2_port
+     = static_cast<ModulePort *>(slave2_cable->getFromPort());
+
+    return hub && master1 && master2 && slave1 && slave2 &&
+           master1_cable && master2_cable && slave1_cable && slave2_cable &&
+           master1_port && master2_port && slave1_port && slave2_port &&
+           hm1_port && hm2_port && hs1_port && hs2_port;
 }
 
 void FlowControl_Master2_Switch_Slave2::initData()
 {
+    FlowControlBase::initData();
 
+    // 初始化属性
+    hub->initData();
+    master1->initData();
+    slave1->initData();
+    slave2->initData();
+    master1_cable->initData();
+    master2_cable->initData();
+    slave1_cable->initData();
+    slave2_cable->initData();
+    
+    // 设置运行数据
+    
+    
+    // 连接信号槽
 }
 
 void FlowControl_Master2_Switch_Slave2::clearData()
 {
-
 }
 
 void FlowControl_Master2_Switch_Slave2::passOneClock()
 {
-
 }
 
 void FlowControl_Master2_Switch_Slave2::refreshUI()
 {
-
 }
--- soc/flowcontrol/flowcontrol_master2_switch_slave2.h
+++ soc/flowcontrol/flowcontrol_master2_switch_slave2.h
@@ -9,12 +9,13 @@
 #define FLOWCONTROL_MASTER2_SWITCH_SLAVE2_H
 
 #include "flowcontrolbase.h"
+#include "switchmodule.h"
 
 class FlowControl_Master2_Switch_Slave2 : public FlowControlBase
 {
 public:
     FlowControl_Master2_Switch_Slave2(GraphicArea *ga, QObject *parent = nullptr);
-    
+
 protected:
     bool initModules() override;
     void initData() override;
@@ -23,10 +24,12 @@ protected:
     void refreshUI() override;
 
 private:
+    SwitchModule *hub;
     MasterModule *master1, *master2;                                          // Master
     SlaveModule *slave1, *slave2;                                             // Slave
     ModuleCable *master1_cable, *master2_cable, *slave1_cable, *slave2_cable; // Master - Slave 连接线
-    ModulePort *master1_port, *master2_port, *slave1_port, *sslave_port;      // Master传输到Slave的端口
+    ModulePort *master1_port, *master2_port, *slave1_port, *slave2_port;      // Master的输出与Slave的输入端口
+    ModulePort *hm1_port, *hm2_port, *hs1_port, *hs2_port;                      // Switch的四个端口
 };
 
 #endif // FLOWCONTROL_MASTER2_SWITCH_SLAVE2_H
